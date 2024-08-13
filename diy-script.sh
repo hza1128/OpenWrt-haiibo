0ebf49d086c7e0c31a4b3bb05f427a9cd8e74579
hza1128 <141570215+hza1128@users.noreply.github.com>
2024-08-13 22:06:39 +0800

    Update diy-script.sh

--- diy-script.sh
+++ diy-script.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# 修改默认IP
-# sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+# 修改默认IP与调整网口顺序
+sed -i 's/192.168.1.1/192.168.10.12/g' package/base-files/files/bin/config_generate
+sed -i "s,'eth1' 'eth0','eth0' 'eth1',g" target/linux/rockchip/armv8/base-files/etc/board.d/02_network
 
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
